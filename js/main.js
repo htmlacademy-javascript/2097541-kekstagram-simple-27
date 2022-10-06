d983f7093ff535ba4126fc89a3f1ac4665bad722
Сергей <Lye4@mail.ru>
2022-10-06 23:46:45 +0300

    исправляет ошибку eslint

--- js/main.js
+++ js/main.js
@@ -5,9 +5,13 @@ function checkStringLength(string, minLength, maxLength) {
   return false;
 }
 
+checkStringLength();
+
 function getRandomNumber(min, max) {
   if (min >= 0 && max >= 0 && min < max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   return 0 / 0;
 }
+
+getRandomNumber();
