c15fcf2cf4355ab34f79b50cbb4263f3bbb9ae42
Сергей <Lye4@mail.ru>
2022-10-10 18:51:23 +0300

    исправляет ошибки

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
-function checkStringLength(string, minLength, maxLength) {
-  if (string.length >= minLength && string.length <= maxLength) {
-    return true;
-  }
-  return false;
-}
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
 
 checkStringLength();
 
-function getRandomNumber(min, max) {
-  if (min >= 0 && max >= 0 && min < max) {
+const getRandomNumber = (min, max) => {
+  const isArgumentsValid = min >= 0 && max >= 0 && min <= max;
+  if (isArgumentsValid) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  return 0 / 0;
-}
+  throw new RangeError('The arguments must be more than zero and the second argument must be greater than the first.');
+};
 
 getRandomNumber();
