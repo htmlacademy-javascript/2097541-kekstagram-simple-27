5b4080e5135a4dd2bb1f685c16d9637673b1b4ac
Keks <keks@htmlacademy.ru>
2022-10-15 21:31:53 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -2,47 +2,45 @@ const checkStringLength = (string, maxLength) => string.length <= maxLength;
 
 checkStringLength('123', 10);
 
-function getRandomNumber(min, max) {
+const getRandomNumber = (min, max) => {
   const isArgumentsValid = min >= 0 && max >= 0 && min <= max;
   if (isArgumentsValid) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   throw new RangeError('The arguments must be more than zero and the second argument must be greater than the first.');
-}
+};
 
 getRandomNumber(1, 10);
 
 
-const photosCount = 25;
+const PHOTOS_COUNT = 25;
 
 const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
 
-const descriptions = [
+const DESCRIPTIONS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.'
 ];
 
-const likesCount = {
-  min: 15,
-  max: 200
+const LikesCount = {
+  MIN: 15,
+  MAX: 200
 };
 
-const commentsCount = {
-  min: 0,
-  max: 200
+const CommentsCount = {
+  MIN: 0,
+  MAX: 200
 };
 
 const createPhoto = (index) => ({
   id: index,
   url: `photos/${index}`,
-  description: getRandomArrayElement(descriptions),
-  likes: getRandomNumber(likesCount.min, likesCount.max),
-  comments: getRandomNumber(commentsCount.min, commentsCount.max)
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomNumber(LikesCount.MIN, LikesCount.MAX),
+  comments: getRandomNumber(CommentsCount.MIN, CommentsCount.MAX)
 });
 
-const getPhotos = () => Array.from({length: photosCount}, (_, photoIndex) => createPhoto(photoIndex + 1));
+const getPhotos = () => Array.from({length: PHOTOS_COUNT}, (_, photoIndex) => createPhoto(photoIndex + 1));
 
-
-getRandomArrayElement(descriptions);
 getPhotos();
