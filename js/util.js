0511b98543db3510d82653d5d9d3ecd38ed27105
Сергей <Lye4@mail.ru>
2022-10-18 22:40:03 +0300

    разбивает код по модулям

---
+++ js/util.js
@@ -0,0 +1,15 @@
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
+
+checkStringLength('123', 10);
+
+const getRandomNumber = (min, max) => {
+  const isArgumentsValid = min >= 0 && max >= 0 && min <= max;
+  if (isArgumentsValid) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  throw new RangeError('The arguments must be more than zero and the second argument must be greater than the first.');
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+export {getRandomNumber, getRandomArrayElement};
