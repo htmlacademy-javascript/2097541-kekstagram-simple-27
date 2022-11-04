0467cdf8caf137d509e0a787b53f85a4c3429678
Keks <keks@htmlacademy.ru>
2022-11-04 16:46:32 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,7 @@
+import {addPhotos} from './miniatures.js';
+import {openUploadModal, closeUploadModal} from './form.js';
+
+addPhotos();
+
+openUploadModal();
+closeUploadModal();
---
+++ 6/js/miniatures.js
@@ -0,0 +1,30 @@
+import {getPhotos} from './data.js';
+
+const photosList = document.querySelector ('.pictures');
+const photoTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const photos = getPhotos();
+
+const photosListFragment = document.createDocumentFragment();
+
+const createElement = (element) => element.cloneNode(true);
+
+const changeElement = (element, value) => {
+  element.querySelector('.picture__img').src = value.url;
+  element.querySelector('.picture__comments').textContent = value.comments;
+  element.querySelector('.picture__likes').textContent = value.likes;
+  return element;
+};
+
+const createPhotos = (data) => {
+  data.forEach ((photo) => {
+    const photoElement = createElement(photoTemplate);
+    changeElement(photoElement, photo);
+    photosListFragment.appendChild(photoElement);
+  });
+  return photosListFragment;
+};
+
+const addPhotos = () => photosList.appendChild(createPhotos(photos));
+
+export {addPhotos};
