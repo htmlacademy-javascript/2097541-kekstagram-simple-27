6f2c77ea910579167fdad98776cbc81c255fd48f
Сергей <Lye4@mail.ru>
2022-11-16 00:56:43 +0300

    исправляет нейминг

--- js/photo-scale.js
+++ js/photo-scale.js
@@ -4,23 +4,23 @@ const scaleBigger = document.querySelector('.scale__control--bigger');
 const scaleValue = document.querySelector('.scale__control--value');
 const imgPreview = document.querySelector('.img-upload__preview').querySelector('img');
 
-const minPhotoSize = 25;
-const maxPhotoSize = 100;
-const stepPhotoSize = 25;
+const MIN_PHOTO_SIZE = 25;
+const MAX_PHOTO_SIZE = 100;
+const STEP_PHOTO_SIZE = 25;
 
 const onScaleSmaller = () => {
   const value = scaleValue.value.slice(0, -1);
-  if (value > minPhotoSize) {
-    scaleValue.value = `${value - stepPhotoSize}%`;
-    imgPreview.style.transform = `scale(${(value - stepPhotoSize) / 100})`;
+  if (value > MIN_PHOTO_SIZE) {
+    scaleValue.value = `${value - STEP_PHOTO_SIZE}%`;
+    imgPreview.style.transform = `scale(${(value - STEP_PHOTO_SIZE) / 100})`;
   }
 };
 
 const onScaleBigger = () => {
   const value = scaleValue.value.slice(0, -1);
-  if (value < maxPhotoSize) {
-    scaleValue.value = `${value - (- stepPhotoSize)}%`;
-    imgPreview.style.transform = `scale(${(value - (- stepPhotoSize)) / 100})`;
+  if (value < MAX_PHOTO_SIZE) {
+    scaleValue.value = `${value - (- STEP_PHOTO_SIZE)}%`;
+    imgPreview.style.transform = `scale(${(value - (- STEP_PHOTO_SIZE)) / 100})`;
   }
 };
 
