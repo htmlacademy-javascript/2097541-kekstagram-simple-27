ede153edb95a977ca49096647c2b609dde1cf009
Сергей <Lye4@mail.ru>
2022-11-21 16:54:39 +0300

    исправляет ошибки

--- js/api.js
+++ js/api.js
@@ -1,13 +1,13 @@
 
+const SERVER_ADDRES = 'https://27.javascript.pages.academy/kekstagram-simple';
+
 const getData = (onSuccess) => {
-  const SERVER_ADDRES = 'https://27.javascript.pages.academy/kekstagram-simple/data';
-  fetch(SERVER_ADDRES)
+  fetch(`${SERVER_ADDRES}/data`)
     .then((response) => response.json())
     .then((photos) => {onSuccess(photos);});
 };
 
 const sendData = (onSuccess, onFail, body) => {
-  const SERVER_ADDRES = 'https://27.javascript.pages.academy/kekstagram-simple';
   fetch(
     SERVER_ADDRES,
     {
